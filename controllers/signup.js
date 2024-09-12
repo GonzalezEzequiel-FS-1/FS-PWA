b2d8b649de24807774e6317953525857a2cd4356
Zeke <egonzalezhidalgo@student.fullsail.edu>
2024-09-12 01:45:59 +0000

    Developing DB interaction

--- controllers/signup.js
+++ controllers/signup.js
@@ -1,9 +1,27 @@
-const signup = (req, res) => {
-  const { email, phone, birthday, password } = req.body;
-  res.json({
-    message: "User has sucessfully signed up!",
-    user: { email, phone, birthday, password: "●●●●●●●●●●●" },
-  });
+const Users = require('../models/users.js');
+const bcrypt = require('bcrypt'); 
+
+const signup = async (req, res) => {
+  try {
+    const { email, phone, birthday, password } = req.body;
+  
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await Users.create({ email, phone, birthday, password: hashedPassword });
+
+    res.json({
+      message: "User has successfully signed up!",
+      user: { email, phone, birthday, password: "●●●●●●●●●●●" }, 
+      data: newUser
+    });
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: `${req.method} failed, consult error >>> ${error}`
+    });
+  }
 };
 
 module.exports = { signup };
